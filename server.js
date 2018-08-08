c45abb0642da2011696ff58130085974823c59a3
Rakib-31 <37634514+Rakib-31@users.noreply.github.com>
2018-08-08 09:53:25 +0600

    updated

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var departments = require('./routes/departments');
 var members = require('./routes/members');
 var researches = require('./routes/researches');
 var citations = require('./routes/citations');
+var teachers = require('./routes/teachers');
 
 var port = 2000;
 
@@ -29,6 +30,7 @@ app.use('/api', departments);
 app.use('/api', members);
 app.use('/api', researches);
 app.use('/api', citations);
+app.use('/api', teachers);
 
 app.listen(port, function(){
 	console.log('Server started on port ' +port);
